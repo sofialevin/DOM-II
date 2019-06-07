28a0979e5c89c49885d236668b13f2757178793a
sofialevin <slevin@M-4876.local>
2019-06-06 23:31:18 -0700

    Added bus animation on mouseover.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const funBackground = document.querySelector('.fun-background');
+const funBusFirst = document.querySelector('.fun-bus-initial');
+const funBus = document.querySelector('.fun-bus');
+funBackground.addEventListener('mouseover', () => {
+    funBusFirst.classList.add('initial-fun-bus-moving');
+    funBus.classList.add('fun-bus-moving');
+})
