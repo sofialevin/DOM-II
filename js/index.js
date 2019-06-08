69527d6d1a1e9dcfa3cc6e549facc9320f36bceb
sofialevin <slevin@M-4876.local>
2019-06-08 10:41:14 -0700

    Added onscroll and load events.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 
+// mouseover event
 const funBackground = document.querySelector('.fun-background');
 const funBusFirst = document.querySelector('.fun-bus-initial');
 const funBus = document.querySelector('.fun-bus');
@@ -7,3 +8,19 @@ funBackground.addEventListener('mouseover', () => {
     funBusFirst.classList.add('initial-fun-bus-moving');
     funBus.classList.add('fun-bus-moving');
 })
+
+//onload event
+
+const scrollIndicator = document.querySelector('.chevron');
+
+window.onload = () => {
+    setTimeout(function() {
+        scrollIndicator.style.display = 'block';
+    }, 8000);
+  };
+
+  // scroll event
+
+  window.addEventListener('scroll', function() {
+    scrollIndicator.style.display = 'none';
+  });
